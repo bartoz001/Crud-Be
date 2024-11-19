e7402ac47813f51ff2c11e2a91f8cf1203970247
bartoz001 <gautampbx1@gmail.com>
2024-11-19 12:42:47 +0530

    this is on working

--- Routs/Auth.js
+++ Routs/Auth.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { Register , Login } from "../controlers/Auth.js";
+import { Register , Login,UserProfile } from "../controlers/Auth.js";
 
 const router = express.Router();
 
 router.post("/register", Register)
 router.post("/login", Login)
+router.get("/profile/:user_id", UserProfile)
 export default router;
